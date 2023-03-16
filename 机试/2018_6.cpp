25f7a402de53ae22437e3d3a9c9d68ff9f64b483
Lelouch-Lirong <56789630+Lelouch-Lirong@users.noreply.github.com>
2023-03-16 22:45:17 +0800

    Add files via upload

---
+++ 机试/2018_6.cpp
@@ -0,0 +1,33 @@
+/*
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+
+bool is_(char *str)
+{
+    int len = strlen(str);
+    int i = 0, j = len-1;
+    while(i < j){
+        if(str[i] != str[j])    return false;
+        i++, j--;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    char tmp;
+    getchar();
+    while(n--){
+        char str[100];
+        cin.getline(str, 100);
+        //cout<<str<<endl;
+        if(is_(str))    cout<<"yes"<<endl;
+        else cout<<"no"<<endl;
+    }
+    return 0;
+}
+*/
---
+++ 机试/2019_5.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+
+struct date{
+    int year;
+    int month;
+    int day;
+};
+
+int day(struct date A, struct date B){
+
+
+
+
+}
