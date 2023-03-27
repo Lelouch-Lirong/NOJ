0eeea023697d5b24c0208f4f6d8b1dee1529946c
lirong <lirongleiyang@163.com>
2023-03-27 20:29:11 +0800

    19.cpp

---
+++ data_structure/19.cpp
@@ -0,0 +1,49 @@
+/*
+基于图的深度优先搜索策略
+*/
+#include<iostream>
+#define MAX 200
+using namespace std;
+
+bool visit[MAX]={false};
+class map{
+    private:
+        int m, n;            //n:顶点数  m：边数
+        int vi[MAX];        //顶点信息
+        int ver[MAX][2];    //边的信息
+    public:
+        map();
+        bool deep_serch(int vi, int vj);
+};
+map::map(){
+    cin>>n>>m;
+    for(int i = 0; i < n; i++){
+        cin>>vi[i];
+    }
+    for(int i = 0; i < m; i++){
+        cin>>ver[i][0]>>ver[i][1];
+    }
+}
+bool map::deep_serch(int vi, int vj){
+    for(int i = 0; i < m; i++){
+        if(ver[i][0] == vi){
+            visit[vi] = true;
+            if(ver[i][1] == vj){
+                return true;
+            }else{
+                if(!visit[ver[i][1]] && deep_serch(ver[i][1], vj))   return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    map M;
+    int vi, vj;
+    cin>>vi>>vj;
+    if(M.deep_serch(vi, vj))    cout<<"yes"<<endl;
+    else                        cout<<"no"<<endl;
+    return 0;
+}
