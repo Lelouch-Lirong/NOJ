bb60398551f8425ef54b197aef6824370a8ce273
lirong <lirongleiyang@163.com>
2023-03-28 15:15:57 +0800

    20.cpp

---
+++ data_structure/20.cpp
@@ -0,0 +1,58 @@
+/*
+基于图的广度优先搜索
+*/
+#include<iostream>
+#include<queue>
+#define MAX 200
+using namespace std;
+
+bool visit[MAX]={false};
+queue<int> Q;
+class map{
+    private:
+        int m, n;            //n:顶点数  m：边数
+        int vi[MAX];        //顶点信息
+        int ver[MAX][2];    //边的信息
+    public:
+        map();
+        bool BFS(int vj);
+};
+map::map(){
+    cin>>n>>m;
+    for(int i = 0; i < n; i++){
+        cin>>vi[i];
+    }
+    for(int i = 0; i < m; i++){
+        cin>>ver[i][0]>>ver[i][1];
+    }
+}
+bool map::BFS(int vj){
+    int vi = Q.front();
+    Q.pop();
+    visit[vi] = true;
+    for(int i = 0; i < m; i++){
+        if(ver[i][0] == vi){
+            if(ver[i][1] == vj){
+                return true;
+            }else{
+                if(!visit[ver[i][1]]){
+                    Q.push(ver[i][1]);
+                    visit[vi] = true;
+                }
+            }
+        }
+    }
+    if(Q.empty())    return false;
+    else             return BFS(vj);
+}
+
+int main()
+{
+    map M;
+    int vi, vj;
+    cin>>vi>>vj;
+    Q.push(vi);
+    if(M.BFS(vj))               cout<<"yes"<<endl;
+    else                        cout<<"no"<<endl;
+    return 0;
+}
